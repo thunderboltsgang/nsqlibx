ce51d08d90a76ab640073aad51971d8896f94044
Authapon R. Kongwan <authapon@gmail.com>
2022-12-12 16:26:16 +0700

    init

--- main.go
+++ main.go
@@ -10,15 +10,19 @@ import (
 type (
 	ConsumeHandler struct {
 		QChannel chan []byte
+		Mutex    sync.Mutex
 	}
 )
 
 var (
-	QServer []string = make([]string, 0, 0)
-	Qmutex  sync.Mutex
+	QServer   []string = make([]string, 0, 0)
+	SecretTxt string   = ""
+	Qmutex    sync.Mutex
 )
 
 func (q *ConsumeHandler) HandleMessage(m *nsq.Message) error {
+	q.Mutex.Lock()
+	defer q.Mutex.Unlock()
 
 	if len(m.Body) == 0 {
 		return nil
@@ -30,19 +34,21 @@ func (q *ConsumeHandler) HandleMessage(m *nsq.Message) error {
 }
 
 func AddQServer(serv string) {
-	go func() {
-		Qmutex.Lock()
-		defer Qmutex.Unlock()
+	Qmutex.Lock()
+	defer Qmutex.Unlock()
 
-		QServer = append(QServer, serv)
-	}()
+	QServer = append(QServer, serv)
 }
 
 func GetChannel(topic string, channel string) chan []byte {
+	log.Printf("%s\n", "start to create channel")
 	handler := new(ConsumeHandler)
 	handler.QChannel = make(chan []byte)
+
 	for serv := range QServer {
+		log.Printf("%s\n", "starting to create consumer")
 		go func(server string) {
+			log.Printf("connecting to %s\n", server)
 			config := nsq.NewConfig()
 			consumer, err := nsq.NewConsumer(topic, channel, config)
 			if err != nil {
