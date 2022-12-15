568b0e9fc6d97a28892af378faa450ad2414e467
Authapon Kongwan <authapon@gmail.com>
2022-12-15 19:33:13 +0700

    init

--- main.go
+++ main.go
@@ -12,20 +12,25 @@ import (
 
 type (
 	ConsumeHandler struct {
-		QChannel chan []byte
-		Mutex    sync.Mutex
+		QChannel    chan []byte
+		ExitChannel chan bool
+		Consumers   []*nsq.Consumer
+		Running     bool
+		Mutex       sync.RWMutex
 	}
 
 	nsqlogger struct{}
 )
 
 var (
-	QServer      []string = make([]string, 0, 0)
-	SecretTxt    string   = ""
-	Qmutex       sync.RWMutex
-	randomx      *rand.Rand    = rand.New(rand.NewSource(time.Now().UnixNano()))
-	PollInterval time.Duration = time.Second
-	logging      bool          = false
+	QServer         []string = make([]string, 0, 0)
+	SecretTxt       string   = ""
+	Qmutex          sync.RWMutex
+	randomx         *rand.Rand    = rand.New(rand.NewSource(time.Now().UnixNano()))
+	PollInterval    time.Duration = time.Second
+	logging         bool          = false
+	TimeoutDuration time.Duration = time.Minute
+	RandomTopicLen  int           = 20
 )
 
 func (q *ConsumeHandler) HandleMessage(m *nsq.Message) error {
@@ -57,6 +62,10 @@ func AddQServer(serv string) {
 	QServer = append(QServer, serv)
 }
 
+func SetTimeOut(t time.Duration) {
+	TimeoutDuration = t
+}
+
 func SetPollInterval(intv time.Duration) {
 	PollInterval = intv
 }
@@ -66,8 +75,32 @@ func Logging(t bool) {
 }
 
 func GetChannel(topic string, channel string) chan []byte {
+	ch, _ := GetChannelWithExit(topic, channel)
+	return ch
+}
+
+func GetChannelWithExit(topic string, channel string) (chan []byte, chan bool) {
 	handler := new(ConsumeHandler)
 	handler.QChannel = make(chan []byte)
+	handler.ExitChannel = make(chan bool)
+	handler.Consumers = make([]*nsq.Consumer, 0)
+	handler.Running = true
+
+	go func() {
+		<-handler.ExitChannel
+
+		handler.Mutex.Lock()
+		handler.Running = false
+		close(handler.QChannel)
+		close(handler.ExitChannel)
+		handler.Mutex.Unlock()
+
+		for cx := range handler.Consumers {
+			go func(cxx int) {
+				handler.Consumers[cxx].Stop()
+			}(cx)
+		}
+	}()
 
 	Qmutex.RLock()
 	defer Qmutex.RUnlock()
@@ -85,6 +118,12 @@ func GetChannel(topic string, channel string) chan []byte {
 			consumer.AddHandler(handler)
 
 			for {
+				handler.Mutex.RLock()
+				if !handler.Running {
+					handler.Mutex.RUnlock()
+					return
+				}
+				handler.Mutex.RUnlock()
 				err := consumer.ConnectToNSQD(server)
 				if err != nil {
 					time.Sleep(PollInterval)
@@ -92,11 +131,13 @@ func GetChannel(topic string, channel string) chan []byte {
 				}
 				break
 			}
-
+			handler.Mutex.Lock()
+			handler.Consumers = append(handler.Consumers, consumer)
+			handler.Mutex.Unlock()
 		}(QServer[serv])
 	}
 
-	return handler.QChannel
+	return handler.QChannel, handler.ExitChannel
 }
 
 func Publish(topic string, mess []byte) error {
@@ -140,4 +181,48 @@ func Publish(topic string, mess []byte) error {
 	}
 
 	return nil
+}
+
+func PublishReply(topic string, mess []byte) []byte {
+	return PublishReplyWithTimeout(topic, mess, TimeoutDuration)
+}
+
+func PublishReplyWithTimeout(topic string, mess []byte, timeout time.Duration) []byte {
+	replyTopic := GetRandomTopic(RandomTopicLen)
+	reply := make(chan interface{})
+	timeoutCh := make(chan bool)
+
+	go func() {
+		ch, exitc := GetChannelWithExit(replyTopic, "reply")
+
+		select {
+		case replyData := <-ch:
+			reply <- replyData
+		case <-timeoutCh:
+			reply <- true
+		}
+		exitc <- true
+	}()
+
+	go func() {
+		time.Sleep(timeout)
+		timeoutCh <- true
+	}()
+
+	getreply := <-reply
+	switch getreply.(type) {
+	case []byte:
+		return getreply.([]byte)
+	case bool:
+	}
+	return make([]byte, 0, 0)
+}
+
+func GetRandomTopic(replylen int) string {
+	alphabet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	output := ""
+	for i := 0; i < len(alphabet); i++ {
+		output += string(alphabet[randomx.Intn(len(alphabet))])
+	}
+	return output
 }
